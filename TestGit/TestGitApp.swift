48e2d87f7d5e5c2502a029077ec23efbe8d77471
Emiko Clark <>
2025-01-26 12:48:12 -0500

    committing the whole project

---
+++ TestGit/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  TestGit
+//
+//  Created by Emiko Clark on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
